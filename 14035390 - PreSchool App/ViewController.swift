fed02a26ee1c242937a01874f796d0e3ba778d7b
James Nwankwo <jamesnwankwo@Jamess-iMac-2.local>
2017-03-17 16:24:48 +0000

    Initial Commit

---
+++ 14035390 - PreSchool App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  14035390 - PreSchool App
+//
+//  Created by James Nwankwo on 17/03/2017.
+//  Copyright (c) 2017 James Nwankwo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
